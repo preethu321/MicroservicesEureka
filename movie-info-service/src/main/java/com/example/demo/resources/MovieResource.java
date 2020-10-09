15a72acd1e70c3c222824b96375caf4182541832
preethu321 <preethu.93.s@gmail.com>
2020-10-10 00:09:45 +0530

    Added microservice project to git

---
+++ movie-info-service/src/main/java/com/example/demo/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package com.example.demo.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Movie;
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie("Test Movie", movieId);
+		
+	}
+}
---
+++ movie-info-service/src/test/java/com/example/demo/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootTest
+@EnableEurekaClient
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
